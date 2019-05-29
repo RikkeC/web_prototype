80be860fd1d789bb9cbe124ba66e35c1e6a8f6a7
Rikke Christiansen <rikkechristiansen@Rikkes-MacBook-Pro.local>
2019-05-29 12:26:37 +0200

    alle sidder næsten færdig

--- script.js
+++ script.js
@@ -12,26 +12,28 @@ function sidenVises() {
 function toggleMenu() {
 
     console.log("toggleMenu");
-    document.querySelector("#togglemenu").classList.remove("hidden");
+    document.querySelector("section").classList.toggle("hidden_none");
+    document.querySelector("#togglemenu").classList.toggle("hidden");
 
-    document.querySelector(".title").classList.add("hidden");
+    document.querySelector("#headerright").classList.toggle("hidden");
 
-    document.querySelector("#menuknap").addEventListener("click", backAgain);
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
 }
 
 function dropdown_tema1() {
     console.log("dropdown_tema1");
-    document.querySelector("#dropdown_tema1").classList.remove("hidden_none");
-    document.querySelector("#down1").addEventListener("click", backAgain);
-
+    document.querySelector("#dropdown_tema1").classList.toggle("hidden_none");
+    document.querySelector("#down1").classList.toggle("hidden");
+    document.querySelector("#op").classList.toggle("hidden");
+    document.querySelector("#op").addEventListener("click", dropdown_tema1);
 }
-
-function backAgain() {
-    console.log("backAgain");
-    document.querySelector("#togglemenu").classList.add("hidden");
-
-
-    document.querySelector(".title").classList.remove("hidden");
-    document.querySelector("#dropdown_tema1").classList.add("hidden_none");
-}
+//
+//function backAgain() {
+//    console.log("backAgain");
+//    document.querySelector("#togglemenu").classList.add("hidden");
+//
+//
+//    document.querySelector(".title").classList.remove("hidden");
+//    document.querySelector("#dropdown_tema1").classList.add("hidden_none");
+//}
